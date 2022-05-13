735328921d6ff9e081e0ff3944318a8bc92247ad
kamy1997 <kamykechir@gmail.com>
2022-05-14 00:14:22 +0200

    changes

--- test_api.py
+++ test_api.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 import unittest
 import requests
 class TestApi(unittest.TestCase):
@@ -8,20 +9,27 @@ class TestApi(unittest.TestCase):
     def test_1_get_all(self):
         resp = requests.get(self.URL)
         self.assertEqual(resp.status_code, 200)
-        self.assertEqual(len(resp.json()), 1)
+        
         print ("Test one is completed")
 
+    def test_get_one(self):
+        resp = requests.get('http://127.0.0.1:5000/api/Riyad')
+        self.assertEqual(resp.status_code, 200)
+        
+        print ("Test is completed")
+
     def test_2_post(self):
-        resp = requests.post(self.URL, json=self.data)
+        resp = requests.post(self.URL, json={"name":"Maya","age":"16","birthday":"12/09/2006"})
         self.assertEqual(resp.status_code, 200)
        
         print ("Test two is completed")
 
 
     def test_update(self):
-         resp = requests.put(self.URL)
+         resp = requests.put('http://127.0.0.1:5000/api/Alix',json={"name":"Riyad","age":"17","birthday":"14/09/1997"})
          respGet = requests.get(self.URL)
          self.assertEqual(respGet.status_code, 200)   
+         print("good")
 
         
 
